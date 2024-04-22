39b76853306412f97675bf723c7e7741e5842fb0
wvdh <wanda@vandenhoogen.eu>
2024-04-22 20:55:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ toggleSwitch.addEventListener('change', () => {
 //Function for generation random passwords.
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-const generateBtnEl = document.getElementById("generate-password-btn")
 const code1El = document.getElementById("code1-el")
 const code2El = document.getElementById("code2-el")
 let password1 = []
 let password2 = []
-
+const generateBtnEl = document.getElementById("generate-password-btn")
 generateBtnEl.addEventListener("click", () => {
     let password1 = getRandomPassword()
     let password2 = getRandomPassword()
